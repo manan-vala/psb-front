6c612f5243c38bfc400decf45bb3a62ea74696c0
Manan <mananvala15@gmail.com>
2026-07-22 22:59:51 +0530

    agentation removed

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { Agentation } from "agentation"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    {/* {import.meta.env.DEV && <Agentation />} */}
-    {<Agentation />}
+    {import.meta.env.DEV && <Agentation />}
+    {/* {<Agentation />} */}
   </StrictMode>,
 )
